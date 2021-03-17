e5c064596f50f2ccfb47f587e6ef4c107d81b40a
Octavian Contis <bmx_tavi@yahoo.com>
2021-03-17 17:39:01 +0000

    Open weather API integration

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+require("dotenv").config();
 var app = express();
 
 // view engine setup
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var router = express.Router();
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/weather', function(req,res){
+  let zipcode = req.body.location;
+  let apikey = process.env.API_KEY;
+  let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${apikey}`;
+  request(url, function(err, response, body) {
+    console.log(JSON.parse(body));
+    res.render("weather", {title: "Weather"});
+  });
 });
 
 module.exports = router;
